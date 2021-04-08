d940b960896d82a1e34b17a9dac27fa19a31c6fc
Debadrita438 <bristibandyopadhyay@gmail.com>
2021-04-08 11:15:29 +0530

    Initial Deploy

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click='start' :disabled='isPlaying'>Let's Play!</button>
+  <Block v-if='isPlaying' :delay='delay' @end='endGame' />
+  <Results v-if='showResult' :score='score' />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Block from './components/Block'
+  import Results from './components/Results'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: { Block, Results },
+    data() {
+      return {
+        isPlaying: false,
+        delay: null,
+        score: null,
+        showResult: false
+      }
+    },
+    methods: {
+      start() {
+        this.delay = 2000 + Math.random() * 5000
+        this.isPlaying = true
+        this.showResult = false
+      },
+      endGame(reactionTime) {
+        this.score = reactionTime
+        this.isPlaying = false
+        this.showResult = true
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #c9d1d9;
+    margin-top: 60px;
+  }
+  body {
+    background: #0d1117;
+  }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="block" v-if='showBlock' @click='stopTimer'>
+      How Fast Can You Click Me?!
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['delay'],
+        data() {
+            return {
+                showBlock: false,
+                timer: null,
+                reactionTime: 0
+            }
+        },
+        mounted() {
+            setTimeout(() => {
+                this.showBlock = true
+                this.startTimer()
+            }, this.delay)
+        },
+        methods: {
+            startTimer() {
+                this.timer= setInterval(() => {
+                    this.reactionTime += 10
+                }, 10)
+            },
+            stopTimer() {
+                clearInterval(this.timer)
+                this.$emit('end', this.reactionTime)
+            }
+        }
+    }
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background: #077056;
+        color: #c9d1d9;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
